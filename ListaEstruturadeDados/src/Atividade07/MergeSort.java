5f7a9a6b3449c2066f5794bf03119a93b2ea59ff
Henrique Lima <henrique.souza.lima@outlook.com>
2020-11-11 17:50:40 -0300

    Lista de Exercicios 07

---
+++ ListaEstruturadeDados/src/Atividade07/MergeSort.java
@@ -0,0 +1,51 @@
+package Atividade07;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		int v[] = { 26, 69, 25, 53, 59, 27, 41, 0, 33, 16, 35, 43 };
+		int w[] = new int[v.length];
+
+		mergeSort(v, w, 0, v.length - 1);
+		System.out.println(Arrays.toString(v));
+
+	}
+
+	// Definindo o meio do vetor e realizando a divisão por recursão
+	private static void mergeSort(int v[], int w[], int ini, int fim) {
+		if (ini < fim) {
+			int meio = (ini + fim)/2;
+			mergeSort(v, w, ini, meio); // Ordena a primeira metade do vetor
+			mergeSort(v, w, meio+1, fim); // Ordena a segunda metade do vetor
+			intercalar(v, w, ini, meio, fim); // Método para intercalar e unir os vetores ordenados
+		}
+
+	}
+
+	// Realizando a intercalação dos dois vetores ordenados
+	public static void intercalar(int v[], int w[], int ini, int meio, int fim) {
+		for (int k = ini; k <= fim; k++) {
+			w[k] = v[k];
+		}
+
+		int i = ini;
+		int j = meio + 1;
+
+		for (int k = ini; k <= fim; k++) {
+			if (i > meio) {
+				v[k] = w[j++];
+			} else if (j > fim) {
+				v[k] = w[i++];
+
+			} else if (w[i] < w[j]) {
+				v[k] = w[i++];
+			} else {
+				v[k] = w[j++];
+			}
+		}
+
+	}
+
+}
